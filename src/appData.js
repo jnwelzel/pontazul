5aa4b1cf3b37c1b9040f4f2e4dbce8d76743589c
Jon Welzel <jnwelzel@gmail.com>
2017-06-06 07:50:56 -0300

    revertido dados inicias p valores originais

--- src/appData.js
+++ src/appData.js
@@ -23,29 +23,5 @@ export const INITIAL_DATA = [
     "modelo": "Fusca",
     "placa": "PAI-4121",
     "valor": 20000
-  }, {
-    "id": "4",
-    "combustivel": "Gasolina",
-    "imagem": null,
-    "marca": "Ford",
-    "modelo": "Fusion",
-    "placa": "SAS-3312",
-    "valor": 20000
-  }, {
-    "id": "5",
-    "combustivel": "Flex",
-    "imagem": null,
-    "marca": "Audi",
-    "modelo": "TT",
-    "placa": "ZAS-3312",
-    "valor": 89000
-  }, {
-    "id": "6",
-    "combustivel": "Flex",
-    "imagem": null,
-    "marca": "Fiat",
-    "modelo": "Prêmio",
-    "placa": "ADR-0332",
-    "valor": 3000
   }
 ];
